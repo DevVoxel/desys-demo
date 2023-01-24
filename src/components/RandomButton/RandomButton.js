25d0b15109546c542254c342ff6cd725c4059690
Voxel20 <aidensmith20@gmail.com>
2023-01-23 20:08:44 -0500

    Update

--- src/components/RandomButton/RandomButton.js
+++ src/components/RandomButton/RandomButton.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import sample from 'lodash/sample';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import sample from "lodash/sample";
 
-import './RandomButton.css';
+import "./RandomButton.css";
 
 /**
  * Button that changes label on every click.
  */
 // eslint-disable-next-line import/prefer-default-export
 export class RandomButton extends Component {
-	static propTypes = {
-		/**
-		 * List of possible labels.
-		 */
-		variants: PropTypes.array.isRequired,
-	};
+  static propTypes = {
+    /**
+     * List of possible labels.
+     */
+    variants: PropTypes.array.isRequired,
+  };
 
-	constructor(props) {
-		super();
-		this.state = {
-			label: sample(props.variants),
-		};
-	}
+  constructor(props) {
+    super();
+    this.state = {
+      label: sample(props.variants),
+    };
+  }
 
-	handleClick = () => {
-		this.setState({
-			label: sample(this.props.variants),
-		});
-	};
+  handleClick = () => {
+    this.setState({
+      label: sample(this.props.variants),
+    });
+  };
 
-	render() {
-		return (
-			<button className="random-button" onClick={this.handleClick}>
-				{this.state.label}
-			</button>
-		);
-	}
+  render() {
+    return (
+      <button className="random-button" onClick={this.handleClick}>
+        {this.state.label}
+      </button>
+    );
+  }
 }
